904446324a128d65917c4d312b61408be727a19a
kaushikp9 <kaushikprakash94@gmail.com>
2023-06-07 12:19:49 +0530

    change in points table

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const teamRows = document.querySelectorAll('.team');
+const teamRows = document.querySelectorAll(".team");
 
 teamRows.forEach((teamRow) => {
-  const noCell = teamRow.querySelector('.no');
+  const noCell = teamRow.querySelector(".no");
   const no = noCell.textContent;
-  const teamNameCell = teamRow.querySelector('.team-name');
+  const teamNameCell = teamRow.querySelector(".team-name");
   const teamName = teamNameCell.textContent;
-  const winsCell = teamRow.querySelector('.wins');
+  const winsCell = teamRow.querySelector(".wins");
   const wins = winsCell.textContent;
-  const lossesCell = teamRow.querySelector('.losses');
+  const lossesCell = teamRow.querySelector(".losses");
   const losses = lossesCell.textContent;
-  const drawsCell = teamRow.querySelector('.draws');
+  const drawsCell = teamRow.querySelector(".draws");
   const draws = drawsCell.textContent;
-  const pointsCell = teamRow.querySelector('.points');
+  const pointsCell = teamRow.querySelector(".points");
   const points = pointsCell.textContent;
-  const teamDetails = document.createElement('div');
-  teamDetails.classList.add('team-details');
+  const teamDetails = document.createElement("div");
+  teamDetails.classList.add("team-details");
   teamDetails.innerHTML = `<strong>Team ${no}</strong><br>Team: ${teamName}<br>Wins: ${wins}<br>Losses: ${losses}<br>Draws: ${draws}<br>Points: ${points}`;
   teamRow.appendChild(teamDetails);
 
-  teamRow.addEventListener('mouseenter', () => {
-    teamDetails.style.opacity = '1';
-    teamDetails.style.visibility = 'visible';
+  teamRow.addEventListener("mouseenter", () => {
+    teamDetails.style.opacity = "1";
+    teamDetails.style.visibility = "visible";
   });
 
-  teamRow.addEventListener('mouseleave', () => {
-    teamDetails.style.opacity = '0';
-    teamDetails.style.visibility = 'hidden';
+  teamRow.addEventListener("mouseleave", () => {
+    teamDetails.style.opacity = "0";
+    teamDetails.style.visibility = "hidden";
   });
 });
